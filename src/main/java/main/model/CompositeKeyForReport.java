029c12da598b800f4bbf427acda4881abf6da238
Eduard Abrakov <abrakovem@mail.ru>
2019-12-22 19:41:06 +0500

    change entity

---
+++ src/main/java/main/model/CompositeKeyForReport.java
@@ -0,0 +1,57 @@
+package main.model;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class CompositeKeyForReport implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "Номер счета второго порядка по Плану счетов")
+    private Bill bill;
+
+    @ManyToOne
+    @JoinColumn(name = "Регистрационный номер кредитной организации")
+    private Bank bank;
+
+    public CompositeKeyForReport() {
+    }
+
+    public CompositeKeyForReport(Bill bill, Bank bank) {
+        this.bill = bill;
+        this.bank = bank;
+    }
+
+    public Bill getBill() {
+        return bill;
+    }
+
+    public void setBill(Bill bill) {
+        this.bill = bill;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CompositeKeyForReport that = (CompositeKeyForReport) o;
+        return bill.equals(that.bill) &&
+                bank.equals(that.bank);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bill, bank);
+    }
+}
